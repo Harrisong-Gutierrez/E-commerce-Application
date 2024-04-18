6d90004b0e7426c451b68adb9abc9a751a85d933
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-04-18 14:49:22 -0600

    implementing react tostify

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,6 @@ import HeroSections from "@/components/HeroSections";
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
     try {
       createClient();
       return true;
