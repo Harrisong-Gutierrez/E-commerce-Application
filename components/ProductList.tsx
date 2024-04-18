cd752482f3da098655fe84f9b6397ba8f8d4878a
Harrisong <harrygutie7@gmail.com>
2024-04-18 03:49:41 -0600

    implementing product filtering

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProductCard from "./ProductCard";
 import { Product } from "@/utils/supabase/types";
 
@@ -8,8 +8,25 @@ interface ProductListProps {
 }
 
 const ProductList: React.FC<ProductListProps> = ({ products, onAddToCart }) => {
-  function renderproducts() {
-    return products.map((product) => (
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>(products);
+
+
+  useEffect(() => {
+    setFilteredProducts(products);
+  }, [products]);
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const term = event.target.value.toLowerCase();
+    setSearchTerm(term);
+    const filtered = products.filter(product =>
+      product.name.toLowerCase().includes(term)
+    );
+    setFilteredProducts(filtered);
+  };
+
+  function renderProducts() {
+    return filteredProducts.map((product) => (
       <ProductCard
         key={product.id}
         product={product}
@@ -18,14 +35,18 @@ const ProductList: React.FC<ProductListProps> = ({ products, onAddToCart }) => {
     ));
   }
 
-  console.log(products);
-  if (!Array.isArray(products) || products.length === 0) {
-    return <div>No hay productos disponibles.</div>;
-  }
-
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-      {renderproducts()}
+    <div>
+      <input
+        type="text"
+        placeholder="Buscar productos..."
+        value={searchTerm}
+        onChange={handleSearch}
+        className="appearance-none border border-gray-300 rounded-md py-2 px-4 mb-4 w-full text-gray-700 leading-tight focus:outline-none focus:border-indigo-500"
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+        {renderProducts()}
+      </div>
     </div>
   );
 };
